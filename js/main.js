195a30125be0ae9f276a43e645d35709b2bc15f3
SamuelBureloJeronimo <santorosario0608@gmail.com>
2024-02-20 21:30:50 -0600

    Update - Incompleto

--- js/main.js
+++ js/main.js
@@ -13,9 +13,38 @@ function compilar(){
     //let res = compilarLR(CADENA_INPUT.value);
     //resultElement.innerHTML = res;
     identificarPuntos(CADENA_INPUT.value);
+    identificarExponentes(Concat);
 }
+function identificarExponentes(arr){
+    for (let i = 0; i < arr.length; i++) {
+        for (let f = 0; f < arr[i].length; f++) {
+            if(arr[i].slice(f,f+1) === "^"){
+                if(Operadores["Cerradura de Kleene"] === arr[i].slice(f+1,f+2)){
+                    console.log("Es Kleene (*)");
+                } else if(Operadores["Cerradura Positiva"] === arr[i].slice(f+1,f+2)){
+                    console.log("Es Positiva (+)");
+                }
+            }
+        }
+    }
+}
+// CATEGORÍAS LÉXICAS EN UN LENGUAJE REGULAR
+let Operadores = {
+    "Cerradura de Kleene": "*",
+    "Cerradura Positiva": "+",
+    "Concatenacion": ".",
+    "Union": "|"
+};
+let palabrasClaves = {
+    "Cadena vacía": "E"
+}
+let delimitadores = {
+    "Parentesis de inicio": "(",
+    "Parentesis de cierre": ")"
+}
+let Concat = [];
+const MAX_N_FINITO = 4;
 function identificarPuntos(cadena){
-    let Concat = [];
     let p1 = 0;
     for (let i = 0; i < cadena.length; i++) {
         console.log(cadena.slice(i,i+1));
